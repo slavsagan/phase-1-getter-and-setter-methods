9d967335c8803cf3b5b19063acaf5bf65d85f94a
Slav <itiksolution@gmail.com>
2022-11-07 19:17:31 +1100

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your Circle class here
+class Circle {
+  constructor(radius) {
+    this.radius = radius
+  }
+  get diameter() {
+    return this.radius * 2
+  }
+  get circumference() {
+    return Math.PI * (this.radius * 2)
+  }
+  get area() {
+    return Math.PI * (this.radius * this.radius)
+  }
+  set diameter(newDiameter) {
+    this.radius = newDiameter / 2
+  }
+  set circumference(newCircumference) {
+    this.radius = newCircumference / (2 * Math.PI)
+  }
+  set area(newArea) {
+    this.radius = Math.sqrt(newArea / Math.PI)
+  }
+}
+let circle = new Circle(6)
+
+circle.diameter = 36
+circle.circumference = 100
+circle.area = 200
+
+console.log(circle.radius)
